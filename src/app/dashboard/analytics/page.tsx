5135a9a0dcd35562d2591706c4fbace829d121fe
GustavoSaavedra <gustavo9418@gmail.com>
2025-05-14 09:12:52 -0300

    feat: Estructura carpetas

---
+++ src/app/dashboard/analytics/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AnalyticsPage = () => {
+  return <div>AnalyticsPage</div>;
+};
+
+export default AnalyticsPage;
+
+// * Página de estadísticas, actividades, notificaciones
---
+++ src/app/vehicles/[vehiclesID]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const VehiclesID = () => {
+  return <div>VehiclesID</div>;
+};
+
+export default VehiclesID;
+
+// * Ruta dinámica, autos por ID
