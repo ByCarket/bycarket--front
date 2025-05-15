96e6091058e54866b2ede26005cf94b2d44f1979
Frysccou <franespindola71@gmail.com>
2025-05-15 15:37:58 -0300

    refactor: move favicon from public to src/app and update layout
    The favicon has been relocated from the public directory to the src/app directory to better organize static assets. The reference to the favicon in the layout.tsx file has been removed since the favicon is now directly placed in the app directory, which is automatically handled by the framework.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<head>
-				<link rel="icon" href="/favicon.png" />
-			</head>
 			<body className={`${nunitoSans.variable} antialiased`}>
 				<AuthProvider>
 					<Navbar />
