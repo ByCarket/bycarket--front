d9cc8409345a18330a4cebed7482fd283000001c
franco <francoscola@hotmail.com>
2025-05-30 17:31:46 +0200

    implementacion Middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('authToken')?.value;
+  console.log('Token en middleware:', token);
+
+  const isDashboardPath = request.nextUrl.pathname.startsWith('/dashboard');
+
+  if (isDashboardPath && !token) {
+    const loginUrl = new URL('/login', request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*'],
+};
